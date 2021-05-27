da840e3435a019ac104f338fa05fbfda36e08687
Emilia Andrzejewska <nidala@Emilias-MacBook-Pro-2.local>
2021-05-27 15:27:16 +0200

    Add index page

---
+++ scr/index.js
@@ -0,0 +1,46 @@
+import 'bootstrap';
+import renderNav from './nav';
+import renderHome from './home';
+import renderMenu from './menu';
+import renderContact from './contact';
+
+renderNav();
+renderHome();
+
+const tabLinks = document.querySelectorAll('.nav-link');
+const content = document.getElementById('content');
+
+const renderPage = (location, page) => {
+  location.innerHTML = '';
+  page();
+};
+
+const removeActive = () => {
+  tabLinks.forEach((link) => {
+    if (link.classList.contains('active')) {
+      link.classList.remove('active');
+    }
+  });
+};
+
+tabLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    if (e.target.textContent === 'Home') {
+      removeActive();
+      e.target.classList.add('active');
+      renderPage(content, renderHome);
+    }
+
+    if (e.target.textContent === 'Menu') {
+      removeActive();
+      e.target.classList.add('active');
+      renderPage(content, renderMenu);
+    }
+
+    if (e.target.textContent === 'Contact') {
+      removeActive();
+      e.target.classList.add('active');
+      renderPage(content, renderContact);
+    }
+  });
+});
